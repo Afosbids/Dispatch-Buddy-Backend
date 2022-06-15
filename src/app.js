c5b1431e61824dbd55eb3a417714d318de7d5f32
samuelige <samuelige.iyanu@gmail.com>
2022-06-15 08:39:17 +0100

    basic setup

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-const  dotenv = require ("dotenv");
-dotenv.config({ path: ".env" });
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const fileupload = require('express-fileupload');
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
-
-const indexRouter = require ('./routes/index');
 const usersRouter = require ( './routes/users');
+const connectDB = require('./db_config/db');
 
 const app = express();
 
-dotenv.config({ path: 'config.env'})
-
-// view engine setup
-app.set('views', path.join(__dirname,'..', 'views'));
-app.set('view engine', 'ejs');
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(fileupload());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,13 +25,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use('/auth', usersRouter);
+
+
+const database_userName = process.env.DATABASE_USERNAME;
+const database_password = process.env.DATABASE_PASSWORD;
+
+// Connect to MongoDB
+const dbUrl = `mongodb+srv://${database_userName}:${database_password}@cluster0.ir60w.mongodb.net/?retryWrites=true&w=majority`
+connectDB(dbUrl)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+
 
 // error handler
 app.use(function( err, req, res, next) {
