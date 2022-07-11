f2686f326c64ee43e32b1fa0642ee3630be4fa95
stevechude <stevechude@gmail.com>
2022-07-11 13:24:30 +0100

    created orderstatus and payment method

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const connectDB = require('./db_config/db');
 const userAuthRouter = require('./routes/auth/user');
 const orderRoute  = require('./routes/orderRoute');
 const riderRoute  = require('./routes/riderRoute');
+const transactionRoute = require('./routes/transactionRoute');
 
 const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
@@ -40,6 +41,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', userAuthRouter );
 app.use('/api/v1/order', [ orderRoute ] );
 app.use('/api/v1/rider', riderRoute );
+app.use('/', transactionRoute);
 
 // middleware
 const notFoundMiddleware = require('./middleware/notFound');
