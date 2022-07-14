b7ffd1fe885806fe40544c920a86cee77d3c80ef
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-07-14 15:59:27 +0100

    RiderEarnings route

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,33 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({ 
+const orderSchema = new mongoose.Schema(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, 'Please provide user id'],
-        ref: 'User',
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Please provide user id"],
+      ref: "User",
     },
     pickupLocation: {
-        type: String,
-        required: [true, 'Please enter pick up location'],
+      type: String,
+      required: [true, "Please enter pick up location"],
     },
     amount: {
-        type: Number,
-        required: [true, 'Please enter amount'],
+      type: Number,
+      required: [true, "Please enter amount"],
     },
     dropOffLocation: {
-        type: String,
-        required: [true, 'Please enter drop off location'],
+      type: String,
+      required: [true, "Please enter drop off location"],
     },
     dropOffPhoneNumber: {
-        type: String,
-        required: [true, 'Please enter drop off phone number'],
+      type: String,
+      required: [true, "Please enter drop off phone number"],
     },
     orderStatus: {
-        type: String,
-        enum: ['Pending', 'Accepted','Delivered'],
-        default: 'Pending'
+      type: String,
+      enum: ["Pending", "Accepted", "Delivered"],
+      default: "Pending",
     },
-}, { timestamps: true });
+    startTrip: {
+      type: String,
+      default: "",
+    },
+    endTrip: {
+      type: String,
+      default: "",
+    },
+    duration: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
--- src/models/Rider.js
+++ src/models/Rider.js
@@ -1,28 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const riderSchema = new mongoose.Schema({
+const riderSchema = new mongoose.Schema(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, 'Please provide user id'],
-        ref: 'User',
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Please provide user id"],
+      ref: "User",
     },
     city: {
-        type: String,
-        required: [true, 'Please provide city'],
+      type: String,
+      required: [true, "Please provide city"],
     },
     bikeDocument: {
-        type: String,
-        required: [true, 'Please provide bike document'],
+      type: String,
+      required: [true, "Please provide bike document"],
     },
     valid_IdCard: {
-        type: String,
-        required: [true, 'Please provide valid ID card'],
+      type: String,
+      required: [true, "Please provide valid ID card"],
     },
     passport_photo: {
-        type: String,
-        required: [true, 'Please provide passport photo'],
-    }
-}, { timestamps: true });
+      type: String,
+      required: [true, "Please provide passport photo"],
+    },
+    orderHistory: [Object],
+  },
+  { timestamps: true }
+);
 
-const Rider = mongoose.model('Rider', riderSchema);
+const Rider = mongoose.model("Rider", riderSchema);
 module.exports = { Rider };
